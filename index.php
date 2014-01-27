218a7fb22f37fd7640128fbbe6bafd0b1f99cbab
norlin <norlin@gmail.com>
2014-01-27 18:45:12 +0800

    Init

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!--
+ Copyright &copy; 2012 Jabatan Kerja Raya Malaysia
+-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>This is first release of product</h1>
+        <h3>Senarai roti sekaya</h3>
+        <ul>
+            <li>List of product</li>
+            <li>Roti kaya</li>
+            <li>Roti Banggali</li>
+        </ul>
+        
+        <p>Final for version 2</p>
+        <div>TODO write content</div>
+    </body>
+</html>
